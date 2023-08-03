c7f6caef14f2a43f125ef1e6bfc82d963356badf
Collins-Simiyu <simiyucollins67@gmail.com>
2023-08-03 04:46:12 +0300

    Addition of argument.py

--- argument.py
+++ argument.py
@@ -69,16 +69,26 @@ class car():
 # to 100 using yield and therefore generator
 # An infinite generator function that prints
 # next square number. It starts with 1
-def nextSquare():
-    i = 1
-    # An Infinite loop to generate squares
-    while True:
-        yield i*i
-        i += 1  # Next execution resumes
-        # from this point
-# Driver code to test above generator
-# function
-for num in nextSquare():
-    if num > 100:
-        break
-    print(num)
+# def nextSquare():
+#     i = 1
+#     # An Infinite loop to generate squares
+#     while True:
+#         yield i*i
+#         i += 1  # Next execution resumes
+#         # from this point
+# # Driver code to test above generator
+# # function
+# for num in nextSquare():
+#     if num > 100:
+#         break
+#     print(num)
+# A generator function that yields 1 for first time,
+# 2 second time and 3 third time
+# def simpleGeneratorFun():
+#     yield 1
+#     yield 2
+#     yield 3
+ 
+# # Driver code to check above generator function
+# for value in simpleGeneratorFun():
+#     print(value)
