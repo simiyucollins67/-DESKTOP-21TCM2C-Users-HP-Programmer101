dc0620a83f478aa288fbd0bee218e8a2040988b5
Collins-Simiyu <simiyucollins67@gmail.com>
2023-10-25 21:24:39 +0300

    Collins Adding some new bengohub files

---
+++ .css/js/greeting.js
@@ -0,0 +1,11 @@
+let greeting = "|Welcome to Jvascript";
+console.log(greeting);
+
+var watertemp = 60;
+if (watertemp > 20) {
+  console.log("The water is warm");
+} else if (watertemp >= 0 && watertemp <= 19) {
+  console.log("The temperature is okay");
+} else {
+  console.log("The temperature is too cold or hot");
+}
